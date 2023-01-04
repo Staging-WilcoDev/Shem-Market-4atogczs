1779e9ed502efd6399f0b34b74ab867d012d6761
Vanessa Cooper <82947329+vanessa-cooper@users.noreply.github.com>
2023-01-04 16:48:09 +0200

    Updating Engine URL

--- backend/lib/event.rb
+++ backend/lib/event.rb
@@ -2,7 +2,7 @@ module Event
   def sendEvent(eventName, metadata)
     wilcoId = ENV['WILCO_ID'] || File.read(Rails.root.join("../.wilco"))
     conn = Faraday.new(
-      url: "https://engine.wilco.gg/users/#{wilcoId}/",
+      url: "https://shem.ngrok.io/users/#{wilcoId}/",
       headers: {'Content-Type' => 'application/json'}
     )
 
